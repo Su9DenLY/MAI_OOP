2cc882598f2ce912e6e328fa5bb4fdadf5dc20db
Sky <terebaev1234@gmail.com>
2023-10-21 05:29:44 +0300

    added a rhomb realization

---
+++ lw3/rhomb.cpp
@@ -0,0 +1,102 @@
+#include "rhomb.hpp"
+
+#define POINT_COUNT 4
+
+bool Rhomb::check() const {
+    double side1 = distance(coordinates[0], coordinates[1]);
+    double side2 = distance(coordinates[1], coordinates[2]);
+    double side3 = distance(coordinates[2], coordinates[3]);
+    double side4 = distance(coordinates[3], coordinates[0]);
+
+    return (side1 == side2) && (side2 == side3) && (side3 == side4);
+}
+
+Rhomb::Rhomb(const Point &p0, const Point &p1, const Point &p2, const Point &p3) {
+    coordinates[0] = p0;
+    coordinates[1] = p1;
+    coordinates[2] = p2;
+    coordinates[3] = p3;
+    if (!(this->check())) {
+        throw std::invalid_argument("This figure is not a rhomb");
+    }
+}
+
+void Rhomb::output(std::ostream &os) const noexcept {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        os << coordinates[i] << " ";
+    }
+}
+
+void Rhomb::input(std::istream &is) noexcept {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        is >> coordinates[i];
+    }
+
+    if (!(this->check())) {
+        throw std::invalid_argument("This figure is not a rhomb");
+    }
+}
+
+Rhomb::Rhomb(const Rhomb &other) {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        this->coordinates[i] = other.coordinates[i];
+    }
+}
+
+Rhomb::Rhomb(Rhomb &&other) noexcept {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        this->coordinates[i] = other.coordinates[i];
+    }
+}
+
+Rhomb &Rhomb::operator=(const Rhomb &other) {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        this->coordinates[i] = other.coordinates[i];
+    }
+
+    return *this;
+}
+
+Rhomb &Rhomb::operator=(Rhomb &&other) {
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        this->coordinates[i] = other.coordinates[i];
+    }
+
+    return *this;
+}
+
+Rhomb::operator double() const noexcept {
+    return this->square();
+}
+
+std::ostream &operator<<(std::ostream &os, const Rhomb &obj) {
+    obj.output(os);
+    return os;
+}
+
+std::istream &operator>>(std::istream &is, Rhomb &obj) {
+    obj.input(is);
+    return is;
+}
+
+bool Rhomb::operator==(const Rhomb &other) const {
+    return (*this).square() == other.square();
+}
+
+bool Rhomb::operator!=(const Rhomb &other) const {
+    return !(*this == other);
+}
+
+double Rhomb::square() const noexcept {
+    return distance(coordinates[0], coordinates[2]) * distance(coordinates[1], coordinates[3]) * 0.5;
+}
+
+Point Rhomb::center() const noexcept {
+    double x_center = 0.0, y_center = 0.0;
+    for (int i = 0; i < POINT_COUNT; ++i) {
+        x_center += coordinates[i].get_x();
+        y_center += coordinates[i].get_y();
+    }
+
+    return Point(x_center * 0.25, y_center * 0.25);
+}
---
+++ lw3/rhomb.hpp
@@ -0,0 +1,37 @@
+#ifndef RHOMB_HPP
+#define RHOMB_HPP
+
+#include "figure.hpp"
+
+class Rhomb final : public Figure {
+private:
+    Point coordinates[4];
+
+public:
+    bool check() const;
+
+    Rhomb() = default;
+    Rhomb(const Point &p0, const Point &p1, const Point &p2, const Point &p3);
+    ~Rhomb() = default;
+
+    void output(std::ostream &os) const noexcept override;
+    void input(std::istream &is) noexcept override;
+
+    Rhomb(const Rhomb &other);
+    Rhomb(Rhomb &&other) noexcept;
+    Rhomb &operator=(const Rhomb &other);
+    Rhomb &operator=(Rhomb &&other);
+
+    explicit operator double() const noexcept override;
+
+    friend std::ostream &operator<<(std::ostream &os, const Rhomb &obj);
+    friend std::istream &operator>>(std::istream &is, Rhomb &obj);
+
+    bool operator==(const Rhomb &other) const;
+    bool operator!=(const Rhomb &other) const;
+
+    double square() const noexcept override;
+    Point center() const noexcept override;
+};
+
+#endif
